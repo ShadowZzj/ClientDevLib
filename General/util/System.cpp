2343ffdc024fb7109d0251aee6804b978b379a4b
liuwei23 <liuwei23@kuaishou.com>
2021-12-21 18:24:27 +0800

    mac获取进程uid增加缓存

--- General/util/System.cpp
+++ General/util/System.cpp
@@ -37,29 +37,44 @@ std::string GetActiveUserSid()
         return "";
     return str::Wstr2Str(explorerInfo.UserSid);
 #else
+    
+    static std::mutex lck;
+    std::lock_guard<std::mutex> guard(lck);
+    static std::map<std::string ,std::string> userSidMap;
     std::string activeUserName = GetActiveConsoleUserName();
+    
     if(activeUserName.empty())
         return "";
 
-    std::vector<std::string> args;
-    args.push_back("localhost");
-    args.push_back("-read");
-    args.push_back("/Search/Users/" + activeUserName);
-    args.push_back("GeneratedUID");
-    args.push_back("|");
-    args.push_back("cut");
-    args.push_back("-c15-");
-    args.push_back("\r\n");
-    std::wstring out;
-    int result = zzj::Process::CreateUserProcess("/usr/bin/dscl", activeUserName.c_str(), args, out);
-    if (0 != result)
-        return "";
+    auto it = userSidMap.find(activeUserName);
+    if(it == userSidMap.end())
+    {
+        std::vector<std::string> args;
+        args.push_back("localhost");
+        args.push_back("-read");
+        args.push_back("/Search/Users/" + activeUserName);
+        args.push_back("GeneratedUID");
+        args.push_back("|");
+        args.push_back("cut");
+        args.push_back("-c15-");
+        std::wstring out;
+        int result = zzj::Process::CreateUserProcess("/usr/bin/dscl", activeUserName.c_str(), args, out);
+        if (0 != result)
+            return "";
 
-    if (auto iter = out.find(L"\n"); iter != std::wstring::npos)
-        out.erase(iter);
-    std::string ret = WstrToUTF8Str(out);
-    // libconfig must begin with letter.
-    ret                              = std::string("p-") + ret;
-    return ret;
+        if (auto iter = out.find(L"\n"); iter != std::wstring::npos)
+            out.erase(iter);
+        std::string ret = WstrToUTF8Str(out);
+        ret = std::string("p-") + ret;
+        
+        
+        userSidMap.emplace(activeUserName, ret);
+        return ret;
+    }
+    else
+    {
+        return it->second;
+    }
+    
 #endif // _WIN32
 }
