9c9b29aa91feab13cef563b6881dc837935e010a
liuwei <liuwei23@kuaishou.com>
2022-03-15 16:54:14 +0800

    增加 export.cmake

---
+++ General/util/ConfigManager/export.cmake
@@ -0,0 +1,16 @@
+set(PRO_SUP $ENV{ZV_PRO_SUP})
+
+include (${PRO_SUP}/General/ThirdParty/sqlite3pp/export.cmake)
+file(GLOB _CONFIGMANAGER_SRCS 
+${PRO_SUP}/General/util/ConfigManager/*.h
+${PRO_SUP}/General/util/ConfigManager/*.cpp
+)
+
+file(GLOB CONFIGMANAGER_SRCS
+${_CONFIGMANAGER_SRCS}
+${SQLITE3PP_INCLUDES}
+${SQLITE3PP_SRCS}
+)
+
+
+source_group(ConfigManager FILES  ${_CONFIGMANAGER_SRCS})
