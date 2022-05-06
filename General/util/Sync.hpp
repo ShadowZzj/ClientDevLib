bbf005acd546cfd0055d95f6b26cbc6b21a5cfb1
zhuzhengjia <491694003@qq.com>
2022-05-06 18:13:27 +0800

    add CheckForStop millisecond

--- General/util/Sync.hpp
+++ General/util/Sync.hpp
@@ -31,8 +31,12 @@ class StopSyncWrapper
     }
     ControlStatus CheckForStop(int seconds)
     {
-        std::unique_lock<std::mutex> lck(m_param.mutex);
-        bool res = m_param.threadCondition.wait_for(lck, std::chrono::seconds(seconds),
+        CheckForStop(std::chrono::milliseconds(1000*seconds));
+    }
+    ControlStatus CheckForStop(std::chrono::milliseconds ms)
+    {
+         std::unique_lock<std::mutex> lck(m_param.mutex);
+        bool res = m_param.threadCondition.wait_for(lck, std::chrono::milliseconds(ms),
                                                     [this]() { return this->m_param.requestStop; });
         return res == false ? ControlStatus::Timeout : ControlStatus::RequestStop;
     }
