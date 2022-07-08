b6176a51ef8ff9d2dd29541e2cd85046edf40299
zhuzhengjia <zhuzhengjia@kuaishou.com>
2022-07-08 14:59:37 +0800

    windows network adapter add info

--- General/util/Network/NetworkAdapter.h
+++ General/util/Network/NetworkAdapter.h
@@ -7,12 +7,40 @@ namespace zzj
 class NetworkAdapter
 {
   public:
+    enum class Type
+    {
+        Wireless,
+        Wired,
+        Loopback,
+        Other
+    };
+    enum class NetworkStatus:unsigned long
+    {
+        Up = 1,
+        DOWN
+    };
     static std::vector<NetworkAdapter> GetNetworkAdapters();
 
-    std::vector<std::string> ipAddrs;
+    std::vector<std::string> ipv4UniAddrs;
+    std::vector<std::string> ipv4AnyAddrs;
+    std::vector<std::string> ipv4MultAddrs;
+    std::vector<std::string> ipv6UniAddrs;
+    std::vector<std::string> ipv6AnyAddrs;
+    std::vector<std::string> ipv6MultAddrs;
+    std::vector<std::string> ipv4DnsAddrs;
+    std::vector<std::string> ipv6DnsAddrs;
+    std::vector<std::string> ipv4GatewayAddrs;
+    std::vector<std::string> ipv6GatewayAddrs;
+
+    unsigned long flags;
+    unsigned long mtu;
+    NetworkStatus status;
+    std::string dnsSuffixes;
     std::string macAddr;
     std::string name;
+    std::string friendlyName;
     std::string description;
+    Type adapterType;
 };
 class NetworkHelper
 {
--- Windows/util/Network/NetworkAdapter.cpp
+++ Windows/util/Network/NetworkAdapter.cpp
@@ -1,5 +1,6 @@
 #include <General/util/Network/NetworkAdapter.h>
 #include <General/util/BaseUtil.hpp>
+#include <General/util/StrUtil.h>
 #include <Winsock2.h>
 #include <ws2tcpip.h>
 #include <IPTypes.h>
@@ -12,63 +13,179 @@ using namespace zzj;
 
 std::vector<NetworkAdapter> NetworkAdapter::GetNetworkAdapters()
 {
-    PIP_ADAPTER_INFO AdapterInfo = NULL;
-    DEFER{
-        if (AdapterInfo)
-            free(AdapterInfo);
+    std::vector<NetworkAdapter> adapters;
+    ULONG size              = 1024 * 15;
+    PIP_ADAPTER_ADDRESSES p = (IP_ADAPTER_ADDRESSES *)HeapAlloc(GetProcessHeap(), 0, size);
+    if (!p)
+        return {};
+    DEFER
+    {
+        HeapFree(GetProcessHeap(), 0, p);
     };
+    ULONG ret;
+    do
+    {
+        ret = GetAdaptersAddresses(AF_UNSPEC, GAA_FLAG_INCLUDE_PREFIX, NULL, p, &size);
+        if (ret != ERROR_BUFFER_OVERFLOW)
+            break;
 
-    DWORD dwBufLen = sizeof(IP_ADAPTER_INFO);
-    char* mac_addr = (char*)malloc(18);
-    DEFER{
-        free(mac_addr);
-    };
-    std::vector<NetworkAdapter> ret;
+        PIP_ADAPTER_ADDRESSES newp = (IP_ADAPTER_ADDRESSES *)HeapReAlloc(GetProcessHeap(), 0, p, size);
+        if (!newp)
+        {
+            return {};
+        }
 
-    AdapterInfo = (IP_ADAPTER_INFO*)malloc(sizeof(IP_ADAPTER_INFO));
-    if (AdapterInfo == NULL) {
-        free(mac_addr);
+        p = newp;
+    } while (true);
+
+    if (ret != NO_ERROR)
+    {
         return {};
     }
 
-    // Make an initial call to GetAdaptersInfo to get the necessary size into the dwBufLen variable
-    if (GetAdaptersInfo(AdapterInfo, &dwBufLen) == ERROR_BUFFER_OVERFLOW) {
-        free(AdapterInfo);
-        AdapterInfo = (IP_ADAPTER_INFO*)malloc(dwBufLen);
-        if (AdapterInfo == NULL) {
-            free(mac_addr);
-            return {};
+    for (PIP_ADAPTER_ADDRESSES tp = p; tp != NULL; tp = tp->Next)
+    {
+        NetworkAdapter adapter;
+        adapter.name         = tp->AdapterName;
+        if (tp->FriendlyName)
+            adapter.friendlyName = str::w2utf8(tp->FriendlyName);
+        if (tp->Description)
+            adapter.description  = str::w2utf8(tp->Description);
+        switch (tp->IfType)
+        {
+        case IF_TYPE_SOFTWARE_LOOPBACK:
+            adapter.adapterType = NetworkAdapter::Type::Loopback;
+            break;
+        case IF_TYPE_ETHERNET_CSMACD:
+            adapter.adapterType = NetworkAdapter::Type::Wired;
+            break;
+        case IF_TYPE_IEEE80211:
+            adapter.adapterType = NetworkAdapter::Type::Wireless;
+            break;
+        default:
+            adapter.adapterType = NetworkAdapter::Type::Other;
+            break;
         }
-    }
 
-    if (GetAdaptersInfo(AdapterInfo, &dwBufLen) == NO_ERROR) {
-        // Contains pointer to current adapter info
-        PIP_ADAPTER_INFO pAdapterInfo = AdapterInfo;
-        do {
-            NetworkAdapter adapter;
-            // technically should look at pAdapterInfo->AddressLength
-            //   and not assume it is 6.
-            sprintf_s(mac_addr, 18, "%02X:%02X:%02X:%02X:%02X:%02X",
-                pAdapterInfo->Address[0], pAdapterInfo->Address[1],
-                pAdapterInfo->Address[2], pAdapterInfo->Address[3],
-                pAdapterInfo->Address[4], pAdapterInfo->Address[5]);
-            adapter.macAddr = mac_addr;
-
-            PIP_ADDR_STRING ipInfo = &pAdapterInfo->IpAddressList;
-            while (ipInfo != nullptr)
+        for (PIP_ADAPTER_UNICAST_ADDRESS pu = tp->FirstUnicastAddress; pu != NULL; pu = pu->Next)
+        {
+            // if ipv4
+            if (pu->Address.lpSockaddr->sa_family == AF_INET)
             {
-                adapter.ipAddrs.push_back(ipInfo->IpAddress.String);
-                ipInfo = ipInfo->Next;
+                sockaddr_in *si         = (sockaddr_in *)(pu->Address.lpSockaddr);
+                char a[INET_ADDRSTRLEN] = {};
+                if (inet_ntop(AF_INET, &(si->sin_addr), a, sizeof(a)))
+                    adapter.ipv4UniAddrs.push_back(a);
             }
+            // if ipv6
+            else if (pu->Address.lpSockaddr->sa_family == AF_INET6)
+            {
+                sockaddr_in6 *si         = (sockaddr_in6 *)(pu->Address.lpSockaddr);
+                char a[INET6_ADDRSTRLEN] = {};
+                if (inet_ntop(AF_INET6, &(si->sin6_addr), a, sizeof(a)))
+                    adapter.ipv6UniAddrs.push_back(a);
+            }
+        }
+        for (PIP_ADAPTER_ANYCAST_ADDRESS pa = tp->FirstAnycastAddress; pa != NULL; pa = pa->Next)
+        {
+            // if ipv4
+            if (pa->Address.lpSockaddr->sa_family == AF_INET)
+            {
+                sockaddr_in *si         = (sockaddr_in *)(pa->Address.lpSockaddr);
+                char a[INET_ADDRSTRLEN] = {};
+                if (inet_ntop(AF_INET, &(si->sin_addr), a, sizeof(a)))
+                    adapter.ipv4AnyAddrs.push_back(a);
+            }
+            // if ipv6
+            else if (pa->Address.lpSockaddr->sa_family == AF_INET6)
+            {
+                sockaddr_in6 *si         = (sockaddr_in6 *)(pa->Address.lpSockaddr);
+                char a[INET6_ADDRSTRLEN] = {};
+                if (inet_ntop(AF_INET6, &(si->sin6_addr), a, sizeof(a)))
+                    adapter.ipv6AnyAddrs.push_back(a);
+            }
+        }
 
-            adapter.name = pAdapterInfo->AdapterName;
-            adapter.description = pAdapterInfo->Description;
-            ret.push_back(adapter);
-            pAdapterInfo = pAdapterInfo->Next;
-        } while (pAdapterInfo);
+        for (PIP_ADAPTER_MULTICAST_ADDRESS pm = tp->FirstMulticastAddress; pm != NULL; pm = pm->Next)
+        {
+            // if ipv4
+            if (pm->Address.lpSockaddr->sa_family == AF_INET)
+            {
+                sockaddr_in *si         = (sockaddr_in *)(pm->Address.lpSockaddr);
+                char a[INET_ADDRSTRLEN] = {};
+                if (inet_ntop(AF_INET, &(si->sin_addr), a, sizeof(a)))
+                    adapter.ipv4MultAddrs.push_back(a);
+            }
+            // if ipv6
+            else if (pm->Address.lpSockaddr->sa_family == AF_INET6)
+            {
+                sockaddr_in6 *si         = (sockaddr_in6 *)(pm->Address.lpSockaddr);
+                char a[INET6_ADDRSTRLEN] = {};
+                if (inet_ntop(AF_INET6, &(si->sin6_addr), a, sizeof(a)))
+                    adapter.ipv6MultAddrs.push_back(a);
+            }
+        }
+        for (PIP_ADAPTER_DNS_SERVER_ADDRESS pd = tp->FirstDnsServerAddress; pd != NULL; pd = pd->Next)
+        {
+            // if ipv4
+            if (pd->Address.lpSockaddr->sa_family == AF_INET)
+            {
+                sockaddr_in *si         = (sockaddr_in *)(pd->Address.lpSockaddr);
+                char a[INET_ADDRSTRLEN] = {};
+                if (inet_ntop(AF_INET, &(si->sin_addr), a, sizeof(a)))
+                    adapter.ipv4DnsAddrs.push_back(a);
+            }
+            // if ipv6
+            else if (pd->Address.lpSockaddr->sa_family == AF_INET6)
+            {
+                sockaddr_in6 *si         = (sockaddr_in6 *)(pd->Address.lpSockaddr);
+                char a[INET6_ADDRSTRLEN] = {};
+                if (inet_ntop(AF_INET6, &(si->sin6_addr), a, sizeof(a)))
+                    adapter.ipv6DnsAddrs.push_back(a);
+            }
+        }
+
+        for (PIP_ADAPTER_GATEWAY_ADDRESS_LH pg = tp->FirstGatewayAddress; pg != NULL; pg = pg->Next)
+        {
+            // if ipv4
+            if (pg->Address.lpSockaddr->sa_family == AF_INET)
+            {
+                sockaddr_in *si         = (sockaddr_in *)(pg->Address.lpSockaddr);
+                char a[INET_ADDRSTRLEN] = {};
+                if (inet_ntop(AF_INET, &(si->sin_addr), a, sizeof(a)))
+                    adapter.ipv4GatewayAddrs.push_back(a);
+            }
+            // if ipv6
+            else if (pg->Address.lpSockaddr->sa_family == AF_INET6)
+            {
+                sockaddr_in6 *si         = (sockaddr_in6 *)(pg->Address.lpSockaddr);
+                char a[INET6_ADDRSTRLEN] = {};
+                if (inet_ntop(AF_INET6, &(si->sin6_addr), a, sizeof(a)))
+                    adapter.ipv6GatewayAddrs.push_back(a);
+            }
+        }
+        if (tp->DnsSuffix)
+            adapter.dnsSuffixes = str::w2utf8(tp->DnsSuffix);
+        adapter.flags       = tp->Flags;
+        adapter.mtu         = tp->Mtu;
+        if (tp->OperStatus == IfOperStatusUp)
+            adapter.status = NetworkAdapter::NetworkStatus::Up;
+        else
+            adapter.status = NetworkAdapter::NetworkStatus::DOWN;
+
+         if (tp->PhysicalAddressLength != 0)
+        {
+            // Get mac address hex string
+            char mac_addr[19] = {0};
+            sprintf_s(mac_addr, 18, "%02X:%02X:%02X:%02X:%02X:%02X", tp->PhysicalAddress[0], tp->PhysicalAddress[1],
+                      tp->PhysicalAddress[2], tp->PhysicalAddress[3], tp->PhysicalAddress[4], tp->PhysicalAddress[5]);
+            adapter.macAddr = mac_addr;
+        }
+        else
+            adapter.macAddr = "";
+        adapters.push_back(adapter);
     }
-
-    return ret; // caller must free.
+    return adapters;
 }
 int NetworkHelper::GetOutIpAddress(std::string &ipAddr,const std::string& toIP)
 {
