daefdcdaa27e166fa1d330d407179a1bcf88b48c
zhuzhengjia <491694003@qq.com>
2022-07-13 17:09:09 +0800

    lua GetUserType

--- General/util/Lua/LuaUtil.h
+++ General/util/Lua/LuaUtil.h
@@ -2,11 +2,27 @@
 #include <json.hpp>
 #include <sol/sol.hpp>
 #include <General/util/LuaExport.hpp>
+#include <optional>
 namespace zzj
 {
 class LuaUtil
 {
   public:
+    template <class T> static std::optional<sol::usertype<T>> GetUserType(const std::string& name,sol::state &_state)
+    {
+        sol::object obj = _state[name];
+        sol::usertype<T> userType;
+        if (obj == sol::lua_nil)
+        {
+            return {};
+        }
+        else if (obj.get_type() != sol::type::table)
+        {
+            return {};
+        }
+        else
+            return _state[name];
+    }
     static nlohmann::json TableToJson(const sol::table &table);
     static bool CanTableConvertToJsonArray(const sol::table &table);
     static sol::table JsonToTable(sol::state& lua, const nlohmann::json& input);
