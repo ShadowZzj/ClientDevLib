342e270219fd92ab7af385aada940340c3c80fba
liuwei23 <liuwei23@kuaishou.com>
2022-10-19 14:17:57 +0800

    grpc release版本

--- General/ThirdParty/grpc/mac/arm/lib/cmake/c-ares/c-ares-targets-release.cmake
+++ General/ThirdParty/grpc/mac/arm/lib/cmake/c-ares/c-ares-targets-release.cmake
@@ -1,15 +1,15 @@
 #----------------------------------------------------------------
-# Generated CMake target import file.
+# Generated CMake target import file for configuration "Release".
 #----------------------------------------------------------------
 
 # Commands may need to know the format version.
 set(CMAKE_IMPORT_FILE_VERSION 1)
 
-# Import target "c-ares::cares" for configuration ""
-set_property(TARGET c-ares::cares APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+# Import target "c-ares::cares" for configuration "Release"
+set_property(TARGET c-ares::cares APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(c-ares::cares PROPERTIES
-  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "C"
-  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libcares.a"
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libcares.a"
   )
 
 list(APPEND _IMPORT_CHECK_TARGETS c-ares::cares )
