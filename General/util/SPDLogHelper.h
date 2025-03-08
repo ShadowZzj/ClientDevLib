7cbb2f7abc6e003c459238162d79cf8fb17ea5e4
zhuzhengjia <zhuzhengjia@xiaohongshu.com>
2025-03-08 17:38:47 +0800

    log模块修复 单参数错误问题

--- General/util/SPDLogHelper.h
+++ General/util/SPDLogHelper.h
@@ -23,38 +23,83 @@
 #define CRITICAL "critical"
 #define OFF "off"
 #define FILE_NAME std::filesystem::path(__FILE__).filename().string()
-#define LOG_INFO(...) \
-    spdlog::info("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, fmt::format(__VA_ARGS__))
-#define LOG_ERROR(...) \
-    spdlog::error("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, fmt::format(__VA_ARGS__))
-#define LOG_WARN(...) \
-    spdlog::warn("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, fmt::format(__VA_ARGS__))
-#define LOG_DEBUG(...) \
-    spdlog::debug("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, fmt::format(__VA_ARGS__))
 
-#define LOG_MODULE_INFO(module, ...) \
-if(spdlog::get(module) != nullptr) \
-spdlog::get(module)->info("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, fmt::format(__VA_ARGS__));\
-else \
-spdlog::info("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, fmt::format(__VA_ARGS__))
 
-#define LOG_MODULE_ERROR(module, ...) \
-if(spdlog::get(module) != nullptr) \
-spdlog::get(module)->error("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, fmt::format(__VA_ARGS__));\
-else \
-spdlog::error("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, fmt::format(__VA_ARGS__))
+#define SPDLOG_EXPAND(...) __VA_ARGS__
+#define SPDLOG_CONCAT_(a, b) a##b
+#define SPDLOG_CONCAT(a, b) SPDLOG_CONCAT_(a, b)
 
-#define LOG_MODULE_WARN(module, ...) \
-if(spdlog::get(module) != nullptr) \
-spdlog::get(module)->warn("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, fmt::format(__VA_ARGS__));\
-else \
-spdlog::warn("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, fmt::format(__VA_ARGS__))
 
-#define LOG_MODULE_DEBUG(module, ...) \
-if(spdlog::get(module) != nullptr) \
-spdlog::get(module)->debug("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, fmt::format(__VA_ARGS__));\
-else \
-spdlog::debug("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, fmt::format(__VA_ARGS__))
+#define LOG_MODULE_ARG_COUNT(...) SPDLOG_EXPAND(LOG_MODULE_ARG_COUNT_IMPL(__VA_ARGS__, 5, 4, 3, 2, 1))
+#define LOG_MODULE_ARG_COUNT_IMPL(_1, _2, _3, _4, _5, N, ...) N
+
+#define LOG_MODULE_GENERIC(level, ...) \
+    SPDLOG_EXPAND(SPDLOG_CONCAT(LOG_MODULE_, LOG_MODULE_ARG_COUNT(__VA_ARGS__))(level, __VA_ARGS__))
+
+
+#define LOG_MODULE_2(level, module, msg)                                           \
+    do                                                                             \
+    {                                                                              \
+        auto logger_ = spdlog::get(module);                                        \
+        if (logger_)                                                               \
+        {                                                                          \
+            logger_->level("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, (msg)); \
+        }                                                                          \
+        else                                                                       \
+        {                                                                          \
+            spdlog::level("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, (msg));  \
+        }                                                                          \
+    } while (0)
+
+#define LOG_MODULE_3(level, module, fmt_str, ...)                          \
+    do                                                                     \
+    {                                                                      \
+        auto logger_ = spdlog::get(module);                                \
+        if (logger_)                                                       \
+        {                                                                  \
+            logger_->level("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, \
+                           fmt::format(fmt_str, __VA_ARGS__));             \
+        }                                                                  \
+        else                                                               \
+        {                                                                  \
+            spdlog::level("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__,  \
+                          fmt::format(fmt_str, __VA_ARGS__));              \
+        }                                                                  \
+    } while (0)
+
+#define LOG_MODULE_4 LOG_MODULE_3
+#define LOG_MODULE_5 LOG_MODULE_3
+
+#define LOG_MODULE_INFO(...) LOG_MODULE_GENERIC(info, __VA_ARGS__)
+#define LOG_MODULE_ERROR(...) LOG_MODULE_GENERIC(error, __VA_ARGS__)
+#define LOG_MODULE_WARN(...) LOG_MODULE_GENERIC(warn, __VA_ARGS__)
+#define LOG_MODULE_DEBUG(...) LOG_MODULE_GENERIC(debug, __VA_ARGS__)
+
+#define LOG_GENERIC(level, ...) \
+    SPDLOG_EXPAND(SPDLOG_CONCAT(LOG_, LOG_MODULE_ARG_COUNT(__VA_ARGS__))(level, __VA_ARGS__))
+
+#define LOG_1(level, msg)                                                     \
+    do                                                                        \
+    {                                                                         \
+        spdlog::level("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, (msg)); \
+    } while (0)
+
+#define LOG_2(level, fmt_str, ...) \
+	do                                                                \
+    {                                                                 \
+        spdlog::level("[{}:{}:{}] {}", FILE_NAME, __func__, __LINE__, \
+                      fmt::format(fmt_str, __VA_ARGS__));             \
+    } while (0)
+
+#define LOG_3 LOG_2
+#define LOG_4 LOG_2
+#define LOG_5 LOG_2
+
+
+#define LOG_INFO(...) LOG_GENERIC(info, __VA_ARGS__)
+#define LOG_ERROR(...) LOG_GENERIC(error, __VA_ARGS__)
+#define LOG_WARN(...) LOG_GENERIC(warn, __VA_ARGS__)
+#define LOG_DEBUG(...) LOG_GENERIC(debug, __VA_ARGS__)
 
 class SPDLogHelp
 {
